fb6e9c73d3e6786c45cf12bb979a330208e1b92b
robertdeg <robertonl@gmail.com>
2018-07-13 14:32:37 +0200

    Initial commit for OptimalPaths

---
+++ code/OptimalPaths.hs
@@ -0,0 +1,28 @@
+module OptimalPaths where
+
+import Data.Ratio
+import qualified Data.Map as M
+
+type Distance = Ratio Integer
+
+class WeightedEdge a (Eq b) | a -> b where
+    weight :: a -> Distance
+    source :: a -> b
+    target :: a -> b
+
+longestPaths :: (WeightedEdge a b) -> [a] -> b -> M.Map b a
+longestPaths edges root
+    = M.empty
+    where
+      (xs, ys)  =   
+
+longestPaths' edges distances root
+    | null xs       = distances
+    | otherwise     = longestPaths' (zs ++ ys) distances' (target z)
+    where
+      outgoing edge = source edge == root
+      relaxing edge = distance (source edge) + weight edge > distance (target edge)
+      (xs, ys)      = L.partition (liftA2 outgoing relaxing) edges
+      z:zs          = xs
+
+
