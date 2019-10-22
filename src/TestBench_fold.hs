c034569bf0e16ddd6e8dced54fc8baf6e3ab2e26
Hendrik-0 <30403894+Hendrik-0@users.noreply.github.com>
2019-10-22 18:43:38 +0200

    added ng graph to both the testbench and graphml directory

--- src/TestBench_fold.hs
+++ src/TestBench_fold.hs
@@ -174,3 +174,202 @@ g1  = Graph (M.fromList
               , SDFEdge "b" "c" 0 1 1
               , SDFEdge "c" "a" 4 1 4
               ])
+
+
+step1 = Graph (M.fromList
+              [ hsdfNode "I " 0
+              , hsdfNode "Z " 0
+              , hsdfNode "f1" 1
+              , hsdfNode "f2" 1
+              , hsdfNode "f3" 1
+              , hsdfNode "f4" 1
+              , hsdfNode "g1" 1
+              , hsdfNode "g2" 1
+              , hsdfNode "g3" 1
+              , hsdfNode "g4" 1
+              ])
+              ([SDFEdge "I " "f1" 0 1 1
+              , SDFEdge "I " "f2" 0 1 1
+              , SDFEdge "I " "f3" 0 1 1
+              , SDFEdge "I " "f4" 0 1 1
+              , SDFEdge "f1" "f2" 0 1 1
+              , SDFEdge "f2" "f3" 0 1 1
+              , SDFEdge "f3" "f4" 0 1 1
+              , SDFEdge "f4" "Z " 0 1 1
+
+              , SDFEdge "I " "g1" 0 1 1
+              , SDFEdge "I " "g2" 0 1 1
+              , SDFEdge "I " "g3" 0 1 1
+              , SDFEdge "I " "g4" 0 1 1
+              , SDFEdge "g1" "g2" 0 1 1
+              , SDFEdge "g2" "g3" 0 1 1
+              , SDFEdge "g3" "g4" 0 1 1
+              , SDFEdge "g4" "Z " 0 1 1
+
+              , SDFEdge "Z " "I " 1 1 1
+              ])
+
+step1' = Graph (M.fromList
+              [ hsdfNode "I " 0
+              , hsdfNode "Z " 0
+              , hsdfNode "ft" 4
+              , hsdfNode "g1" 1
+              , hsdfNode "g2" 1
+              , hsdfNode "g3" 1
+              , hsdfNode "g4" 1
+              ])
+              ([SDFEdge "I " "ft" 0 4 4
+              , SDFEdge "ft" "Z " 0 1 1
+
+              , SDFEdge "I " "g1" 0 1 1
+              , SDFEdge "I " "g2" 0 1 1
+              , SDFEdge "I " "g3" 0 1 1
+              , SDFEdge "I " "g4" 0 1 1
+              , SDFEdge "g1" "g2" 0 1 1
+              , SDFEdge "g2" "g3" 0 1 1
+              , SDFEdge "g3" "g4" 0 1 1
+              , SDFEdge "g4" "Z " 0 1 1
+
+              , SDFEdge "Z " "I " 1 1 1
+              ])
+
+
+
+step1'' = Graph (M.fromList
+              [ hsdfNode "I " 0
+              , hsdfNode "Z " 0
+              , hsdfNode "ft" 4
+              , hsdfNode "gt" 4
+              ])
+              ([SDFEdge "I " "ft" 0 4 4
+              , SDFEdge "ft" "Z " 0 1 1
+
+              , SDFEdge "I " "gt" 0 4 4
+              , SDFEdge "gt" "Z " 0 1 1
+
+              , SDFEdge "Z " "I " 1 1 1
+              ])
+
+
+
+step2 = Graph (M.fromList
+              [ hsdfNode "I " 0
+              , hsdfNode "Z " 0
+              , hsdfNode "If" 0
+              , hsdfNode "ff" 1
+              , hsdfNode "Zf" 0
+              , hsdfNode "g1" 1
+              , hsdfNode "g2" 1
+              , hsdfNode "g3" 1
+              , hsdfNode "g4" 1
+              ])
+              ([SDFEdge "I " "If" 0 1 1
+              , SDFEdge "If" "ff" 0 4 1
+              , SDFEdge "ff" "ff" 1 1 1
+              , SDFEdge "ff" "Zf" 0 1 4
+              , SDFEdge "Zf" "If" 1 1 1
+              , SDFEdge "Zf" "Z " 0 1 1
+
+              , SDFEdge "I " "g1" 0 1 1
+              , SDFEdge "I " "g2" 0 1 1
+              , SDFEdge "I " "g3" 0 1 1
+              , SDFEdge "I " "g4" 0 1 1
+              , SDFEdge "g1" "g2" 0 1 1
+              , SDFEdge "g2" "g3" 0 1 1
+              , SDFEdge "g3" "g4" 0 1 1
+              , SDFEdge "g4" "Z " 0 1 1
+
+              , SDFEdge "Z " "I " 1 1 1
+              ])
+
+
+ng = Graph (M.fromList
+              [ hsdfNode "I " 0
+              , hsdfNode "Z " 0
+
+              , hsdfNode "If" 0
+              , hsdfNode "Zf" 0
+              , hsdfNode "f'" 1
+
+              , hsdfNode "Ig" 0
+              , hsdfNode "Zg" 0
+              , hsdfNode "g1" 1
+              , hsdfNode "g2" 1
+
+              , hsdfNode "Ih" 0
+              , hsdfNode "Zh" 0
+              , hsdfNode "h1" 1
+              , hsdfNode "h2" 1
+              , hsdfNode "h3" 1
+              , hsdfNode "h4" 1
+              ])
+              ([SDFEdge "I " "If" 0 1 1
+              , SDFEdge "I " "Ig" 0 1 1
+              , SDFEdge "I " "Ih" 0 1 1
+
+              , SDFEdge "If" "f'" 0 4 1
+              , SDFEdge "f'" "f'" 1 1 1
+              , SDFEdge "f'" "Zf" 0 1 4
+              , SDFEdge "Zf" "If" 1 1 1
+
+              , SDFEdge "Ig" "g1" 0 2 1
+              , SDFEdge "g1" "g2" 0 1 1
+              , SDFEdge "g2" "g1" 1 1 1
+              , SDFEdge "g2" "Zg" 0 1 2
+              , SDFEdge "Zg" "Ig" 1 1 1
+
+              , SDFEdge "Ih" "h1" 0 1 1
+              , SDFEdge "h1" "h2" 0 1 1
+              , SDFEdge "h2" "h3" 0 1 1
+              , SDFEdge "h3" "h4" 0 1 1
+              , SDFEdge "h4" "Zh" 0 1 1
+              , SDFEdge "Zh" "Ih" 1 1 1
+
+              , SDFEdge "Zf" "Z " 0 1 1
+              , SDFEdge "Zg" "Z " 0 1 1
+              , SDFEdge "Zh" "Z " 0 1 1
+
+              , SDFEdge "Z " "I " 1 1 1
+              ])
+
+
+
+ng1 = Graph (M.fromList
+              [ hsdfNode "If" 0
+              , hsdfNode "Zf" 0
+              , hsdfNode "f'" 1
+              ])
+              ([SDFEdge "If" "f'" 0 4 1
+              , SDFEdge "f'" "f'" 1 1 1
+              , SDFEdge "f'" "Zf" 0 1 4
+              , SDFEdge "Zf" "If" 1 1 1
+              ])
+
+ng2 = Graph (M.fromList
+              [ hsdfNode "Ig" 0
+              , hsdfNode "Zg" 0
+              , hsdfNode "g1" 1
+              , hsdfNode "g2" 1
+              ])
+              ([SDFEdge "Ig" "g1" 0 2 1
+              , SDFEdge "g1" "g2" 0 1 1
+              , SDFEdge "g2" "g1" 1 1 1
+              , SDFEdge "g2" "Zg" 0 1 2
+              , SDFEdge "Zg" "Ig" 1 1 1
+              ])
+
+ng3 = Graph (M.fromList
+              [ hsdfNode "Ih" 0
+              , hsdfNode "Zh" 0
+              , hsdfNode "h1" 1
+              , hsdfNode "h2" 1
+              , hsdfNode "h3" 1
+              , hsdfNode "h4" 1
+              ])
+              ([SDFEdge "Ih" "h1" 0 1 1
+              , SDFEdge "h1" "h2" 0 1 1
+              , SDFEdge "h2" "h3" 0 1 1
+              , SDFEdge "h3" "h4" 0 1 1
+              , SDFEdge "h4" "Zh" 0 1 1
+              , SDFEdge "Zh" "Ih" 1 1 1
+              ])
