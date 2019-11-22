41b327d37702e84ded5050fc2109a382616f40da
Hendrik-0 <30403894+Hendrik-0@users.noreply.github.com>
2019-11-22 14:11:46 +0100

    added dotproduct graphs

--- src/TestBench_dotp.hs
+++ src/TestBench_dotp.hs
@@ -35,6 +35,10 @@ g2  = Graph   (M.fromList
               , SDFEdge "Z" "I" 1 1 1
               ])
 
+-----------------------------------------------------------------------------------------
+-- map function
+-----------------------------------------------------------------------------------------
+
 mapG = Graph  (M.fromList
               [ hsdfNode "I" 0
               , hsdfNode "Z" 0
@@ -64,6 +68,19 @@ mapGf = Graph (M.fromList
               , SDFEdge "Z" "I" 1 1 1
               ])
 
+mapGc = Graph (M.fromList
+              [ hsdfNode "I" 0
+              , hsdfNode "Z" 0
+              , hsdfNode "m" 1
+              ])
+              ([SDFEdge "I" "m" 0 1 1
+              , SDFEdge "m" "Z" 0 1 1
+              , SDFEdge "Z" "I" 1 1 1
+              ])
+
+-----------------------------------------------------------------------------------------
+-- fold function
+-----------------------------------------------------------------------------------------
 
 foldG = Graph  (M.fromList
               [ hsdfNode "I" 0
@@ -93,4 +110,72 @@ foldGf = Graph  (M.fromList
               ([SDFEdge "I" "f" 0 1 1
               , SDFEdge "f" "Z" 0 1 1
               , SDFEdge "Z" "I" 1 1 1
+              ])
+
+foldGc = Graph  (M.fromList
+              [ hsdfNode "I" 0
+              , hsdfNode "Z" 0
+              , hsdfNode "f" 1
+              ])
+              ([SDFEdge "I" "f" 0 1 1
+              , SDFEdge "f" "Z" 0 1 1
+              , SDFEdge "Z" "I" 1 1 1
+              ])
+
+-----------------------------------------------------------------------------------------
+-- dotp function
+-----------------------------------------------------------------------------------------
+
+dotpG = Graph  (M.fromList
+              [ hsdfNode "I " 0
+              , hsdfNode "Z " 0
+              , hsdfNode "m1" 1
+              , hsdfNode "m2" 1
+              , hsdfNode "m3" 1
+              , hsdfNode "m4" 1
+              , hsdfNode "f1" 1
+              , hsdfNode "f2" 1
+              , hsdfNode "f3" 1
+              , hsdfNode "f4" 1
+              ])
+              ([SDFEdge "I " "m1" 0 1 1
+              , SDFEdge "I " "m1" 0 1 1
+              , SDFEdge "I " "m2" 0 1 1
+              , SDFEdge "I " "m3" 0 1 1
+              , SDFEdge "I " "m4" 0 1 1
+              , SDFEdge "m1" "f1" 0 1 1
+              , SDFEdge "m2" "f2" 0 1 1
+              , SDFEdge "m3" "f3" 0 1 1
+              , SDFEdge "m4" "f4" 0 1 1
+              , SDFEdge "f1" "f2" 0 1 1
+              , SDFEdge "f2" "f3" 0 1 1
+              , SDFEdge "f3" "f4" 0 1 1
+              , SDFEdge "f4" "Z " 0 1 1
+              , SDFEdge "Z " "I " 1 1 1
+              ])
+
+dotpGf = Graph  (M.fromList
+              [ hsdfNode "I " 0
+              , hsdfNode "Z " 0
+              , hsdfNode "m " 1
+              , hsdfNode "f " 4
+              ])
+              ([SDFEdge "I " "m " 0 1 1
+              , SDFEdge "I " "m " 0 4 4
+              , SDFEdge "m " "f " 0 4 4
+              , SDFEdge "f " "Z " 0 1 1
+              , SDFEdge "Z " "I " 1 1 1
+              ])
+
+dotpGc = Graph  (M.fromList
+              [ hsdfNode "I " 0
+              , hsdfNode "Z " 0
+              , hsdfNode "m " 1
+              , hsdfNode "f " 1
+              ])
+              ([SDFEdge "I " "m " 0 1 1
+              , SDFEdge "I " "m " 0 4 4
+              , SDFEdge "m " "f " 0 4 4
+              , SDFEdge "f " "Z " 0 1 1
+              , SDFEdge "Z " "I " 1 1 1
               ])
