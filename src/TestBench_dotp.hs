5fe6e410e96f554c6ef2093648024b0ce088432c
Hendrik-0 <30403894+Hendrik-0@users.noreply.github.com>
2019-11-22 17:12:05 +0100

    Added dotproduct graphs

--- src/TestBench_dotp.hs
+++ src/TestBench_dotp.hs
@@ -126,7 +126,7 @@ foldGc = Graph  (M.fromList
 -- dotp function
 -----------------------------------------------------------------------------------------
 
-dotpG = Graph  (M.fromList
+dotpG1 = Graph  (M.fromList
               [ hsdfNode "I " 0
               , hsdfNode "Z " 0
               , hsdfNode "m1" 1
@@ -153,6 +153,99 @@ dotpG = Graph  (M.fromList
               , SDFEdge "f4" "Z " 0 1 1
               , SDFEdge "Z " "I " 1 1 1
               ])
+
+
+dotpG2 = Graph  (M.fromList
+              [ hsdfNode "I " 0
+              , hsdfNode "Z " 0
+              , hsdfNode "Im" 0
+              , hsdfNode "Zm" 0
+              , hsdfNode "If" 0
+              , hsdfNode "Zf" 0
+              , hsdfNode "m1" 1
+              , hsdfNode "m2" 1
+              , hsdfNode "m3" 1
+              , hsdfNode "m4" 1
+              , hsdfNode "f1" 1
+              , hsdfNode "f2" 1
+              , hsdfNode "f3" 1
+              , hsdfNode "f4" 1
+              ])
+              ([SDFEdge "I " "Im" 0 1 1
+              
+              , SDFEdge "Im" "m1" 0 1 1
+              , SDFEdge "Im" "m2" 0 1 1
+              , SDFEdge "Im" "m3" 0 1 1
+              , SDFEdge "Im" "m4" 0 1 1
+              , SDFEdge "m1" "Zm" 0 1 1
+              , SDFEdge "m2" "Zm" 0 1 1
+              , SDFEdge "m3" "Zm" 0 1 1
+              , SDFEdge "m4" "Zm" 0 1 1
+
+              , SDFEdge "Zm" "Im" 1 1 1
+              , SDFEdge "Zm" "If" 0 1 1
+
+              , SDFEdge "If" "f1" 0 1 1
+              , SDFEdge "If" "f2" 0 1 1
+              , SDFEdge "If" "f3" 0 1 1
+              , SDFEdge "If" "f4" 0 1 1
+              , SDFEdge "f1" "f2" 0 1 1
+              , SDFEdge "f2" "f3" 0 1 1
+              , SDFEdge "f3" "f4" 0 1 1
+              , SDFEdge "f4" "Zf" 0 1 1
+              , SDFEdge "Zf" "If" 1 1 1
+              , SDFEdge "Zf" "Z " 0 1 1
+
+              , SDFEdge "Z " "I " 1 1 1
+              ])              
+
+
+dotpGm1 = Graph  (M.fromList
+              [ hsdfNode "Im" 0
+              , hsdfNode "Zm" 0
+              , hsdfNode "m1" 1
+              , hsdfNode "m2" 1
+              , hsdfNode "m3" 1
+              , hsdfNode "m4" 1
+              ])
+              ([SDFEdge "Im" "m1" 0 1 1
+              , SDFEdge "Im" "m2" 0 1 1
+              , SDFEdge "Im" "m3" 0 1 1
+              , SDFEdge "Im" "m4" 0 1 1
+              , SDFEdge "m1" "Zm" 0 1 1
+              , SDFEdge "m2" "Zm" 0 1 1
+              , SDFEdge "m3" "Zm" 0 1 1
+              , SDFEdge "m4" "Zm" 0 1 1
+              , SDFEdge "Zm" "Im" 1 1 1
+              ])              
+
+dotpGf1 = Graph  (M.fromList
+              [ hsdfNode "If" 0
+              , hsdfNode "Zf" 0
+              , hsdfNode "f1" 1
+              , hsdfNode "f2" 1
+              , hsdfNode "f3" 1
+              , hsdfNode "f4" 1
+              ])
+              ([SDFEdge "If" "f1" 0 1 1
+              , SDFEdge "If" "f2" 0 1 1
+              , SDFEdge "If" "f3" 0 1 1
+              , SDFEdge "If" "f4" 0 1 1
+              , SDFEdge "f1" "f2" 0 1 1
+              , SDFEdge "f2" "f3" 0 1 1
+              , SDFEdge "f3" "f4" 0 1 1
+              , SDFEdge "f4" "Zf" 0 1 1
+              , SDFEdge "Zf" "If" 1 1 1
+              ])   
+
+
+
+
+
+
+
+
+
 
 dotpGf = Graph  (M.fromList
               [ hsdfNode "I " 0
